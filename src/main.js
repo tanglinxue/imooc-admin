97fa8bec63ba217d9eaf732a83ae9d8b2820f338
tanglinxue <31237473+tanglinxue@users.noreply.github.com>
2022-01-22 16:31:24 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import installElementPlus from './plugins/element'
+// 初始化样式表
+import '@/styles/index.scss'
+
+import installIcons from '@/icons'
+
+const app = createApp(App)
+installElementPlus(app)
+app.use(store).use(router).mount('#app')
+installIcons(app)
